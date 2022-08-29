fe42f9d394be26040beb61906c37017a88648f79
gavin <2623761201@qq.com>
2022-08-29 19:03:19 +0800

    周日复习&图的存储结构实现(邻接矩阵&邻接表)

--- DataStructureImple/Graph/graph.h
+++ DataStructureImple/Graph/graph.h
@@ -1,20 +1,118 @@
 # define MaxVertexNum 100
 typedef char VertexType;            // 顶点的数据类型
 typedef int EdgeType;               // 带权图中边上权值的数据类型
+typedef int InfoType;               // 邻接表法中存储权值的类型
 typedef struct {
     VertexType Vex[MaxVertexNum];   // 顶点表
     EdgeType Edge[MaxVertexNum][MaxVertexNum];  // 邻接矩阵，边表
     int vexnum, arcnum;                         // 图当前的顶点数和弧数
 }MGraph;                            // 无向图
 
-int Adjacent(MGraph G, int x, int y);           // 判断图是否有边(x, y)
-int* Neighbors(MGraph G, int x);                 // 列出图中与结点x邻接的边
+// 邻接表法
+// 边
+typedef struct ArcNode{
+    int adjvex;                     // 边指向那个结点
+    typedef struct ArcNode *next    // 指向下一条边的指针
+    InfoType info;                  // 权值
+}ArcNode;
+// 顶点
+typedef struct VNode{
+    VertexType data;                // 顶点信息
+    ArcNode *first;                 // 第一条边
+}VNode, AdjList[MaxVertexNum];
+// 图
+typedef struct {
+    AdjList vertices;               // 顶点与边
+    int vexnum, arcnum;             // 图当前的顶点数和弧树
+}ALGraph;
+
+int Adjacent(MGraph G, int x, int y)           // 判断图是否有边(x, y)
+{
+    return G.Edge[x][y] != 0;
+}
+int* Neighbors(MGraph G, int x)                 // 列出图中与结点x邻接的边
+{
+    int neigh[G.vexnum];
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < G.vexnum; i++)
+    {
+        if (G.Edge[x][i] != 0)
+        {
+            neigh[j++] = i;
+        }
+        neigh[j] = -1;
+    }
+    return neigh;
+}
 void InsertVertex(MGraph G, int x);             // 在图G中插入顶点x。
 void DeleteVertex(MGraph G, int x);             // 从图G中删除顶点x。
-void AddEdge(MGraph *G, int x, int y);           // 若无向边(x, y)不存在，则向图G中添加该边，
-void RemoveEdge(MGraph *G,int x,int y);          // 若无向边(x, y)存在，则从图G中删除该边。
-int FirstNeighbor(MGraph G, int x);             // 求图G中顶点x的第一个邻接点，若有则返回项点号。若x没有邻接点或图中不存在x，则返回-1.
-int NextNeighbor(MGraph G, int x, int y);       // 假设图G中顶点y是顶点x的一个邻接点，返回除y外顶点x的下一个邻接点的顶点号，若y是x的最后一个邻接点，则返回-1。
-int Get_edge_value(MGraph G, int x, int y);     //  获取图G中边(x，y)对应的权值。
-void Set_edge_value(MGraph *G, int x, int y, int v); // 设置图G中边(x, y)对应的权值为V。
-void IniGraph(MGraph *G);
+void AddEdge(MGraph *G, int x, int y)           // 若无向边(x, y)不存在，则向图G中添加该边，
+{
+    if ((*G).Edge[x][y] == 0)
+    {
+        (*G).Edge[x][y] == 1;
+    }
+}
+void RemoveEdge(MGraph *G,int x,int y)          // 若无向边(x, y)存在，则从图G中删除该边。
+{
+    if ((*G).Edge[x][y] == 1)
+    {
+        (*G).Edge[x][y] = 0;
+    }
+}
+int FirstNeighbor(MGraph G, int x)             // 求图G中顶点x的第一个邻接点，若有则返回项点号。若x没有邻接点或图中不存在x，则返回-1.
+{
+    int i = 0;
+    for (i = 0; i < G.vexnum; i++)
+    {
+        if (G.Edge[x][i] != 0)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int NextNeighbor(MGraph G, int x, int y)       // 假设图G中顶点y是顶点x的一个邻接点，返回除y外顶点x的下一个邻接点的顶点号，若y是x的最后一个邻接点，则返回-1。
+{
+    for (int i = y; i < G.vexnum; i++)
+    {
+        if (G.Edge[x][i] != 0)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int Get_edge_value(MGraph G, int x, int y)     //  获取图G中边(x，y)对应的权值。
+{
+    return G.Edge[x][y];
+}
+void Set_edge_value(MGraph *G, int x, int y, int v) // 设置图G中边(x, y)对应的权值为V。
+{
+    (*G).Edge[x][y] = v;
+}
+void InitGraph(MGraph *G){
+    // 获得顶点数和弧数
+    int vexnum, arcnum;
+    scanf("%d %d", &vexnum, &arcnum);
+    (*G).vexnum = vexnum;
+    (*G).arcnum = arcnum;
+
+    // 初始化顶点表
+    for (int i = 0; i < vexnum; i++)
+    {
+        char c;
+        scanf("%c", &c);
+        (*G).Vex[i] = c;
+    }
+
+    // 添加边
+    for (int i = 0; i < arcnum; i++)
+    {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        AddEdge(G, x, y);
+    }
+    
+}
--- DataStructureImple/LinkList/LinkList.h
+++ DataStructureImple/LinkList/LinkList.h
@@ -24,50 +24,336 @@ typedef struct{
     int next;
 }SLinkList[MAXSIZE];
 
-// 初始化
-// 初始化一个空的链表 (带头节点)
-int InitList(LinkList *L);
+// 初始化一个空的链表 (无头节点)
+// int InitList(LinkList *L){
+//     (*L) = NULL;
+//     return 1;
+// }
+
+
+//// 初始化一个空的链表 (带头节点)
+int InitList(LinkList *L){
+    (*L) = (LNode *)malloc(sizeof(LNode));
+    if ((*L)->next == NULL)
+    {
+        return 0;
+    }
+    (*L)->next = NULL;
+    return 1;
+}
+
+// 判断单链表是否为空 (无头节点)
+// int Empty(LinkList L){
+//     return L == NULL;
+// }
+
 // 判断单链表是否为空 (有头节点)
-int Empty(LinkList L);
+int Empty(LinkList L){
+    return L->next == NULL;
+}
+
 // 求表长度
-int Length(LinkList L);
+int Length(LinkList L){
+    int length = 0;
+    if (L == NULL)
+    {
+        return 0;
+    }
+    LNode *p = L;
+    while (p->next != NULL)
+    {
+        p = p->next;
+        length++;
+    }
+    return length;
+}
 
-// 插入
-// 按位序插入 (有头节点)
-int ListInsert(LinkList *L, int i, int e);
-// 后插操作：在p节点后插入元素e
-int InsertNextNode(LNode *p, int e);
-// 前插操作：在p节点前插入元素e
-int InsertPriorNode(LNode *p, int e);
-
-// 删除
-// 按位序删除 (有头节点)
-int ListDelete(LinkList *L, int i, int *e);
-// 删除指定结点
-int DeleteNode(LNode *p);
-
-// 查找
 // 按位查找 (有头节点)
-LNode *GetElem(LinkList L, int i);
+LNode *GetElem(LinkList L, int i) {
+    if (i < 0)
+    {
+        printf("请输入正确的位序");
+        return L;
+    }
+    int j = 0;
+    LNode *p = L;
+    while (p != NULL && j < i)
+    {
+        p = p->next;
+        j++;
+    }
+    return p;
+}
+
+// 后插操作：在p节点后插入元素e
+int InsertNextNode(LNode *p, int e){
+    LNode *q = (LNode *)malloc(sizeof(LNode));
+    if (p == NULL || q == NULL)
+    {
+        printf("输入节点为空或新节点创建失败");
+        return 0;
+    }
+    q->data = e;
+    q->next = p->next;
+    p->next = q;
+    return 1;
+}
+
+// 基本操作--按位序插入 (有头节点)
+int ListInsert(LinkList *L, int i, int e){
+    LNode *p = GetElem(*L, i-1);
+    return InsertNextNode(p, e);
+}
+
+// 前插操作：在p节点前插入元素e
+int InsertPriorNode(LNode *p, int e){
+    LNode *q = (LNode *)malloc(sizeof(LNode));
+    if (p == NULL || q == NULL)
+    {
+        printf("输入节点为空或新节点创建失败");
+        return 0;
+    }
+    q->data = p->data;
+    p->data = e;
+    q->next = p->next;
+    p->next = q;
+    return 1;
+}
+
+// 基本操作--按位序插入 (无头节点)
+// int ListInsert(LinkList *L, int i, int e){
+//     if (Empty(L))
+//     {
+//         printf("输入的链表为空");
+//         return 0;
+//     }
+//     if (i < 1) {
+//         printf("请输入合法的位序");
+//         return 0;
+//     }
+//     LNode *p = (*L);
+//     LNode *q = (LNode *)malloc(sizeof(LNode));
+//     if (q == NULL)
+//     {
+//         printf("新节点创建失败");
+//         return 0;
+//     }
+//     q->data = e;
+//     if (i == 1){
+//         q->next = (*L);
+//         p = q;
+//     }
+//     while (p != NULL && j < i-1)
+//     {
+//         p = p->next;
+//         j++;
+//     }
+//     if (p == NULL)
+//     {
+//         printf("位序超过链表长度");
+//         return 0;
+//     }
+//     q->next = p->next;
+//     p->next = q;
+//     return 1;
+// }
+
+// 基本操作--按位序删除 (有头节点)
+int ListDelete(LinkList *L, int i, int *e){
+    LNode *p = GetElem(*L, i-1);
+    if (p == NULL || p->next == NULL)
+    {
+        printf("当前位序无元素");
+        return 0;
+    }
+    LNode *q = p->next; // 要删除的节点
+    *e = q->data;
+    p->next = q->next;
+    free(q);
+    return 1;
+}
+
+// 删除指定结点
+int DeleteNode(LNode *p){
+    if (p == NULL)
+    {
+        printf("当前输入的节点为空无法删除");
+        return 0;
+    }
+    LNode *q = p->next;
+    p->data = q->data;
+    p->next = q->next;
+    free(q);
+    return 1;
+}
+
 // 按值查找
-LNode *LocateElem(LinkList L, int e);
+LNode *LocateElem(LinkList L, int e){
+    if (L == NULL) {
+        printf("输入的链表为空");
+        return NULL;
+    }
+    LNode *p = L->next;
+    while (p != NULL && p->data != e)
+    {
+        p = p->next;
+    }
+    return p;
+}
 
 // 建立
 // 尾插法
-LinkList List_TailInsert(LinkList *L);
+LinkList List_TailInsert(LinkList *L){
+    int x;
+    *L = (LinkList)malloc(sizeof(LNode));
+    LNode *p, *q = *L;
+    printf("input x = ");
+    scanf("%d", &x);
+    while (x != -1)
+    {
+        p = (LNode *)malloc(sizeof(LNode));
+        p->data = x;
+        q->next = p;
+        q = p;
+        printf("input x = ");
+        scanf("%d", &x);
+    }
+    q->next = NULL;
+    return *L;
+}
+
 // 头插法
-LinkList List_HeadInsert(LinkList *L);
+LinkList List_HeadInsert(LinkList *L){
+    int x;
+    InitList(L);
+    printf("input x = ");
+    scanf("%d", &x);
+    while (x != -1)
+    {
+        InsertNextNode(*L, x);
+        printf("input x = ");
+        scanf("%d", &x);
+    }
+    return *L;
+}
 
 // 链表逆置
-LinkList Reverse(LinkList *L);
+LinkList Reverse(LinkList *L){
+    LNode *p;
+    p = (*L)->next;
+    (*L)->next = NULL;
+    while (p != NULL)
+    {
+        LNode *temp;
+        temp = p->next;
+        p->next = (*L)->next;       // 插到链表头
+        (*L)->next = p;             // 更新链表头指针
+        p = temp;
+    }
+    return *L;
+}
+
+// 将a1a2...an变为a1ana2an-1...
+void Change_List(LinkList *L){
+    LNode *p, *q, *s, *r;
+    // 寻找中间结点
+    p = q = (*L);
+    while (q->next != NULL)
+    {
+        p = p->next;
+        q = q->next;
+        if(q->next != NULL)
+            q = q->next;
+    }
+    // 此时p就为中间结点
+    q = p->next;
+    p->next = NULL;
+    // 将后半段逆序
+    while (q != NULL)
+    {
+        r = q->next;
+        q->next = p->next;
+        p->next = q;
+        q = r;
+    }
+    // 插入
+    s = (*L)->next;
+    q = p->next;
+    p->next = NULL;
+    while (q != NULL)
+    {
+        r = q->next;
+        q->next = s->next;
+        s->next = q;
+        s = q->next;
+        q = r;
+    }
+}
 
 // 初始化双链表
-int InitDLinkList(DLinkList *L);
+int InitDLinkList(DLinkList *L){
+    (*L) = (DNode *)malloc(sizeof(DNode));
+    if ((*L) == NULL)
+    {
+        printf("头节点创建失败");
+        return 0;
+    }
+    (*L)->next = NULL;
+    (*L)->prior = NULL;
+    return 1;
+}
 // 判断双链表是否为空(带头节点)
-int DEmpty(DLinkList L);
+int DEmpty(DLinkList L){
+    if(L == NULL)
+    {
+        printf("输入的双链表为空");
+        return 0;
+    }
+    return L->next==NULL;
+}
 // 在p节点后插入q节点
-int InsertNextDNode(DNode *p, DNode *q);
+int InsertNextDNode(DNode *p, DNode *q){
+    if (p==NULL || q==NULL)
+    {
+        printf("输入的双链表节点为空");
+        return 0;
+    }
+    q->next = p->next;
+    if (p->next != NULL)
+    {
+        p->next->prior = q;
+    }
+    p->next = q;
+    q->prior = p;
+    return 1;
+}
 // 删除p节点的后继节点
-int DeleteNextDNode(DNode *p);
+int DeleteNextDNode(DNode *p){
+    if (p == NULL)
+    {
+        printf("输入的双链表节点为空");
+        return 0;
+    }
+    DNode *q = p->next;
+    if (q == NULL)
+    {
+        printf("当前节点无后继节点");
+        return 0;
+    }
+    p->next = q->next;
+    if (q->next != NULL)
+    {
+        q->next->prior = p;
+    }
+    free(q);
+    return 1;
+}
 // 删除双链表
-void DestoryDList(DLinkList *L);
+void DestoryDList(DLinkList *L){
+    while ((*L)->next != NULL)
+    {
+        DeleteNextDNode((*L));
+    }
+    free(*L);
+    *L=NULL;
+}
